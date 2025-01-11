003b3e159e84453889ecc9fb59534b0d8b76a400
DenOfEquity <166248528+DenOfEquity@users.noreply.github.com>
2025-01-11 15:07:22 +0000

    Add files via upload

--- scripts/forge_condBlast.py
+++ scripts/forge_condBlast.py
@@ -6,6 +6,7 @@ import modules as modules
 from modules.prompt_parser import SdConditioning
 from modules.script_callbacks import on_cfg_denoiser, remove_current_script_callbacks
 import torch, math, random, time
+from modules.ui_components import InputAccordion
 
 
 #   shuffling and scaling text conds
@@ -23,182 +24,174 @@ class CondBlastForge(scripts.Script):
         return scripts.AlwaysVisible
 
     def ui(self, *args, **kwargs):
-        with gr.Accordion(open=False, label=self.title()):
-            with gr.Row():
-                enabled = gr.Checkbox(value=False, label='Enabled')
-            with gr.Row():
-                shufflePos = gr.Slider(minimum=0.0, maximum=1.0, step=0.01, value=1.0, label='shuffle positive text conds after step')
-                shuffleNeg = gr.Slider(minimum=0.0, maximum=1.0, step=0.01, value=1.0, label='shuffle negative text conds after step')
-            with gr.Row():
-                noisePos = gr.Slider(minimum=0.0, maximum=0.2, step=0.001, value=0.0, label='add noise to positive text conds')
-                noiseNeg = gr.Slider(minimum=0.0, maximum=0.2, step=0.001, value=0.0, label='add noise to negative text conds')
-            with gr.Row():
-                scalePos = gr.Slider(minimum=0.1, maximum=2.0, step=0.005, value=1.0, label='positive prompt weight')
-                scaleNeg = gr.Slider(minimum=0.1, maximum=2.0, step=0.005, value=1.0, label='negative prompt weight')
-            with gr.Row():
-                zeroPos  = gr.Slider(minimum=0.0, maximum=1.0, step=0.01, value=1.0, label='empty positive after step')
-                zeroNegS = gr.Slider(minimum=0.0, maximum=1.0, step=0.01, value=1.0, label='empty negative before step')
-                zeroNegE = gr.Slider(minimum=0.0, maximum=1.0, step=0.01, value=1.0, label='empty negative after step')
+        with InputAccordion(False, label=self.title()) as enabled:
+            with gr.Accordion(open=False, label="controls for Positive"):
+                with gr.Row():
+                    noisePos = gr.Slider(minimum=0.0, maximum=1.0, step=0.001, value=0.0, label='add noise to text conds')
+                    noisePosS = gr.Slider(minimum=0.0, maximum=1.0, step=0.01, value=0.0, label='noise after step')
+                shufflePos = gr.Slider(minimum=0.0, maximum=1.0, step=0.01, value=1.0, label='shuffle text conds after step')
+                scalePos = gr.Slider(minimum=0.1, maximum=2.0, step=0.005, value=1.0, label='prompt weight')
+                zeroPos  = gr.Slider(minimum=0.0, maximum=1.0, step=0.01, value=1.0, label='empty prompt after step')
+            with gr.Accordion(open=True, label="controls for Negative"):
+                with gr.Row():
+                    posNeg   = gr.Slider(minimum=0.0, maximum=1.0, step=0.01, value=0.0, label='blend positive into negative')
+                    posNegS = gr.Slider(minimum=0.0, maximum=1.0, step=0.01, value=0.0, label='blend after step')
+                with gr.Row():
+                    noiseNeg = gr.Slider(minimum=0.0, maximum=1.0, step=0.001, value=0.0, label='add noise to text conds (ICG)')
+                    noiseNegS = gr.Slider(minimum=0.0, maximum=1.0, step=0.01, value=0.0, label='noise after step')
+                shuffleNeg = gr.Slider(minimum=0.0, maximum=1.0, step=0.01, value=1.0, label='shuffle text conds after step')
+                scaleNeg = gr.Slider(minimum=0.1, maximum=2.0, step=0.005, value=1.0, label='prompt weight')
+                with gr.Row():
+                    zeroNegS = gr.Slider(minimum=0.0, maximum=1.0, step=0.01, value=0.0, label='empty negative before step')
+                    zeroNegE = gr.Slider(minimum=0.0, maximum=1.0, step=0.01, value=1.0, label='empty negative after step')
+
+
+        enabled.do_not_save_to_config = True
+        shufflePos.do_not_save_to_config = True
+        shuffleNeg.do_not_save_to_config = True
+        noisePos.do_not_save_to_config = True
+        noiseNeg.do_not_save_to_config = True
+        noisePosS.do_not_save_to_config = True
+        noiseNegS.do_not_save_to_config = True
+        scalePos.do_not_save_to_config = True
+        scaleNeg.do_not_save_to_config = True
+        zeroPos.do_not_save_to_config = True
+        posNeg.do_not_save_to_config = True
+        posNegS.do_not_save_to_config = True
+        zeroNegS.do_not_save_to_config = True
+        zeroNegE.do_not_save_to_config = True
 
         self.infotext_fields = [
-            (enabled, lambda d: enabled.update(value=("cb_enabled" in d))),
+            (enabled, lambda d: d.get("cb_enabled", False)),
             (shufflePos, "cb_shufflePos"),
             (shuffleNeg, "cb_shuffleNeg"),
             (noisePos, "cb_noisePos"),
             (noiseNeg, "cb_noiseNeg"),
+            (noisePosS, "cb_noisePosS"),
+            (noiseNegS, "cb_noiseNegS"),
             (scalePos, "cb_scalePos"),
             (scaleNeg, "cb_scaleNeg"),
-            (zeroPos, "cb_zeroPos"),
+            (zeroPos,  "cb_zeroPos"),
+            (posNeg,   "cb_posNeg"),
+            (posNegS,  "cb_posNegS"),
             (zeroNegS, "cb_zeroNegS"),
             (zeroNegE, "cb_zeroNegE"),
         ]
 
-        return enabled, shufflePos, shuffleNeg, noisePos, noiseNeg, scalePos, scaleNeg, zeroPos, zeroNegS, zeroNegE
-#add empty neg before step
-#shuffle pos continues to be ineffective
+        return enabled, shufflePos, shuffleNeg, noisePos, noiseNeg, noisePosS, noiseNegS, scalePos, scaleNeg, zeroPos, posNeg, posNegS, zeroNegS, zeroNegE
+
     @torch.no_grad()
     def denoiser_callback(self, params):
         is_SDXL = isinstance (params.text_cond, dict)
         lastStep = params.total_sampling_steps - 1
         batchSize = len(params.text_cond['vector']) if is_SDXL else len(params.text_cond)
-        
-#   is it correct to shuffle/weight the cross-attention?
 
-#shuffle messes up sdxl - maybe clips are cat'd, shuffle blends across?
-#mat crossattn need to match
-
-        if is_SDXL:
-            # positive conds
-            if self.zeroPos < 1.0 or self.shufflePos < 1.0 or self.noisePos > 0.0 or self.scalePos != 1.0:
-                cross = params.text_cond['crossattn'][0]
-                empty_cross = self.empty_cond['crossattn'].resize_as_(cross).reshape(cross.shape)
-                if self.zeroPos * lastStep < params.sampling_step:
-                    cross = empty_cross
-                else:
-                    if self.shufflePos * lastStep < params.sampling_step:
-                        indexes = torch.randperm(cross.size(0))
-                        cross = cross[indexes]
-                        del indexes
-                    if self.noisePos > 0.0:
-                        noise = torch.randn_like(cross)
-                        torch.lerp(cross, noise, self.noisePos, out=cross)
-                        del noise
-                    if self.scalePos != 1.0:
-                        torch.lerp(empty_cross, cross, self.scalePos, out=cross)
-                del empty_cross
-                if batchSize == 1:
-                    params.text_cond['crossattn'][0] = cross
-                else:
-                    params.text_cond['crossattn'] = cross.repeat(batchSize)
-                del cross
-            
-            # negative conds
-            if self.zeroNegS > 0.0 or self.zeroNegE < 1.0 or self.shuffleNeg < 1.0 or self.noiseNeg > 0.0 or self.scaleNeg != 1.0:
-                cross = params.text_uncond['crossattn'][0]
-                empty_cross = self.empty_uncond['crossattn'].resize_as_(cross).reshape(cross.shape)
-                if self.zeroNegS * lastStep > params.sampling_step or self.zeroNegE * lastStep < params.sampling_step:
-                    cross = empty_cross
-                else:
-                    #   shuffle
-                    if self.shuffleNeg * lastStep < params.sampling_step:
-                        indexes = torch.randperm(cross.size(0))
-                        cross = cross[indexes]
-                        del indexes
-                    #noise
-                    if self.noiseNeg > 0.0:
-                        noise = torch.randn_like(cross)
-                        torch.lerp(cross, noise, self.noiseNeg, out=cross)
-                        del noise
-                    #   weight
-                    if self.scaleNeg != 1.0:
-                        torch.lerp(empty_cross, cross, self.scaleNeg, out=cross)
-                del empty_cross
-                
-                if batchSize == 1:
-                    params.text_uncond['crossattn'][0] = cross
-                else:
-                    params.text_uncond['crossattn'] = cross.repeat(batchSize)
-                del cross
-
-        else:   #   not sdXL
-            #   positive
-            if self.zeroPos < 1.0 or self.shufflePos < 1.0 or self.noisePos > 0.0 or self.scalePos != 1.0:
+        ##  POSITIVE
+        if self.zeroPos < 1.0 or self.shufflePos < 1.0 or (self.noisePos > 0.0 and self.noisePosS < 1.0) or self.scalePos != 1.0:
+            if is_SDXL:
+                cond = params.text_cond['crossattn'][0]
+                empty = self.empty_cond['crossattn'].resize_as_(cond).reshape(cond.shape)
+            else:
                 cond = params.text_cond[0]
                 empty = self.empty_cond.resize_as_(cond).reshape(cond.shape)
+                
+            if self.zeroPos * lastStep < params.sampling_step:
+                cond = empty
+            else:
+                if self.noisePos > 0.0 and self.noisePosS * lastStep < params.sampling_step:
+                    noise = torch.randn_like(cond) * cond.std()
+                    torch.lerp(cond, noise, self.noisePos, out=cond)
+                    del noise
+                if self.shufflePos * lastStep < params.sampling_step:
+                    indexes = torch.randperm(cond.size(0))
+                    cond = cond[indexes]
+                    del indexes
+                if self.scalePos != 1.0:
+                    torch.lerp(empty, cond, self.scalePos, out=cond)
 
-                if self.zeroPos * lastStep < params.sampling_step:
-                    cond = empty
+            del empty
+            
+            if is_SDXL:
+                if batchSize == 1:
+                    params.text_cond['crossattn'][0] = cond
                 else:
-                    #   shuffle
-                    if self.shufflePos * lastStep < params.sampling_step:
-                        indexes = torch.randperm(cond.size(0))
-                        cond = cond[indexes]
-                        del indexes
-
-                        if self.scalePos == 1.0:   #   filthy hack
-                            self.scalePos = 0.999
-
-                    #noise - should noise be same for all steps? maybe can't be, due to prompt editing
-                    if self.noisePos > 0.0:
-                        noise = torch.randn_like(cond)
-                        torch.lerp(cond, noise, self.noisePos, out=cond)
-                        del noise
-
-                    #   weight
-                    if self.scalePos != 1.0:
-                        torch.lerp(empty, cond, self.scalePos, out=cond)
-                        del empty
+                    params.text_cond['crossattn'] = cond.repeat(batchSize)
+            else:
                 if batchSize == 1:
                     params.text_cond[0] = cond
                 else:
                     params.text_cond = cond.repeat(batchSize)
-                del cond
 
-            #   negative
-            if self.zeroNegS > 0.0 or self.zeroNegE < 1.0 or self.shuffleNeg < 1.0 or self.noiseNeg > 0.0 or self.scaleNeg != 1.0:
+            del cond
+
+        ##  NEGATIVE
+        if self.zeroNegS > 0.0 or self.zeroNegE < 1.0 or self.shuffleNeg < 1.0 or (self.noiseNeg > 0.0 and self.noiseNegS < 1.0)  or self.scaleNeg != 1.0 or (self.posNeg > 0.0 and self.posNegS < 1.0):
+            if is_SDXL and hasattr (params, 'text_uncond'):
+                cond = params.text_uncond['crossattn'][0]
+                empty = self.empty_uncond['crossattn'].resize_as_(cond).reshape(cond.shape)
+            else:
                 cond = params.text_uncond[0]
                 empty = self.empty_uncond.resize_as_(cond).reshape(cond.shape)
+                
+            if self.zeroNegS * lastStep > params.sampling_step or self.zeroNegE * lastStep < params.sampling_step:
+                cond = empty
+            else:
+                #   blend positive
+                if self.posNeg > 0.0 and self.posNegS * lastStep < params.sampling_step:
+                    if is_SDXL:
+                        pos_cond = params.text_cond['crossattn'][0]
+                    else:
+                        pos_cond = params.text_cond[0]
+                    size = min(pos_cond.shape[0], cond.shape[0])
+                    torch.lerp(cond[:size], pos_cond[:size], self.posNeg, out=cond)
+                    del pos_cond
+            
+                #   noise
+                if self.noiseNeg > 0.0 and self.noiseNegS * lastStep < params.sampling_step:
+                    noise = torch.randn_like(cond) * cond.std()
+                    torch.lerp(cond, noise, self.noiseNeg, out=cond)
+                    del noise
+            
+                #   shuffle
+                if self.shuffleNeg * lastStep < params.sampling_step:
+                    indexes = torch.randperm(cond.size(0))
+                    cond = cond[indexes]
+                    del indexes
+            
+                #   weight
+                if self.scaleNeg != 1.0:
+                    torch.lerp(empty, cond, self.scaleNeg, out=cond)
 
-                if self.zeroNegS * lastStep > params.sampling_step or self.zeroNegE * lastStep < params.sampling_step:
-                    cond = empty
+            del empty
+                    
+            if is_SDXL:
+                if batchSize == 1:
+                    params.text_uncond['crossattn'][0] = cond
                 else:
-                    #   shuffle
-                    if self.shuffleNeg * lastStep < params.sampling_step:
-                        indexes = torch.randperm(cond.size(0))
-                        cond = cond[indexes]
-                        del indexes
-
-                        if self.scaleNeg == 1.0:   #   filthy hack
-                            self.scaleNeg = 0.999
-
-                    #noise - should noise be same for all steps? maybe can't be, due to prompt editing
-                    if self.noiseNeg > 0.0:
-                        noise = torch.randn_like(cond)
-                        torch.lerp(cond, noise, self.noiseNeg, out=cond)
-                        del noise
-
-                    #   weight
-                    if self.scaleNeg != 1.0:
-                        torch.lerp(empty, cond, self.scaleNeg, out=cond)
-                        del empty
-
+                    params.text_uncond['crossattn'] = cond.repeat(batchSize)
+            else:
                 if batchSize == 1:
                     params.text_uncond[0] = cond
                 else:
                     params.text_uncond = cond.repeat(batchSize)
-                del cond
+
+            del cond
 
 
     def process(self, params, *script_args, **kwargs):
-        enabled, shufflePos, shuffleNeg, noisePos, noiseNeg, scalePos, scaleNeg, zeroPos, zeroNegS, zeroNegE = script_args
+        enabled, shufflePos, shuffleNeg, noisePos, noiseNeg, noisePosS, noiseNegS, scalePos, scaleNeg, zeroPos, posNeg, posNegS, zeroNegS, zeroNegE = script_args
         if enabled:
             self.shufflePos = shufflePos
             self.shuffleNeg = shuffleNeg
             self.noisePos = noisePos
             self.noiseNeg = noiseNeg
+            self.noisePosS = noisePosS
+            self.noiseNegS = noiseNegS
             self.scalePos = scalePos
             self.scaleNeg = scaleNeg
-            self.zeroPos = zeroPos
+            self.zeroPos  = zeroPos
+            self.posNeg   = posNeg
+            self.posNegS  = posNegS
             self.zeroNegS = zeroNegS
             self.zeroNegE = zeroNegE
 
@@ -209,9 +202,13 @@ class CondBlastForge(scripts.Script):
                 cb_shuffleNeg = shuffleNeg,
                 cb_noisePos = noisePos,
                 cb_noiseNeg = noiseNeg,
+                cb_noisePosS = noisePosS,
+                cb_noiseNegS = noiseNegS,
                 cb_scalePos = scalePos,
                 cb_scaleNeg = scaleNeg,
-                cb_zeroPos = zeroPos,
+                cb_zeroPos  = zeroPos,
+                cb_posNeg   = posNeg,
+                cb_posNegS  = posNegS,
                 cb_zeroNegS = zeroNegS,
                 cb_zeroNegE = zeroNegE,
             ))
